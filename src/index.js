1beaef582034b72c120eec851717c18a274ab17d
LaurentChion <laurent.chion@gmail.com>
2016-11-28 14:44:07 +0100

    Ajout d'un nouveau css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { createStore } from 'redux'
+import sensors from './reducers'
+
+const store = createStore(sensors)
+
+const render = () => {
+  ReactDOM.render(
+    <App
+       update={() => store.dispatch({ type: 'UPDATE' })}
+    />,
+    document.getElementById('root')
+  );
+};
+render();
+store.subscribe(render);

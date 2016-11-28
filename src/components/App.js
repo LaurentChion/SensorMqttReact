f1b0428f114d6ad4c3b7702d3152955349f5708b
LaurentChion <laurent.chion@gmail.com>
2016-11-28 14:52:24 +0100

    Ajout d'une petite image

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import Table from './Table';
-
+import logo from '../logo.svg';
+import '../css/App.css'
 
 class App extends Component {
   static propTypes = {
@@ -11,7 +12,7 @@ class App extends Component {
     return (
         <div className="row">
             <div className="col-sm-12 main">
-                <h2 className="sub-header">Sensors List</h2>
+                <h1 className="sub-header"><img src={logo} className="App-logo" alt="logo" />Sensors List</h1>
                 <div className="table-responsive">
                   <Table update={this.props.update} />
                 </div>
